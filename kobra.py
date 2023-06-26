caa125f3e40b502c9847176d6a7c5509ab5ff66e
tarantofa <137496103+tarantofa@users.noreply.github.com>
2023-06-26 21:20:03 +0200

    Create kobra.py

---
+++ kobra.py
@@ -0,0 +1,9 @@
+# using While loop
+
+Number = int(input("Please Enter any Number: "))
+Count = 0
+while(Number > 0):
+    Number = Number // 10
+    Count = Count + 1
+
+print("\n Number of Digits in a Given Number = %d" %Count)
